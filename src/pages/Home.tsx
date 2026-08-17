b8bb48819529ac06877dbb29aa884f58a63e260f
Jakub-Laska <laska.rayl@gmail.com>
2026-08-17 21:07:15 +0200

    feat: add favicon start app and navbar

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import "../css/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <form className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Home;

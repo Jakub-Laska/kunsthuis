cd7b27120c3990f6ebbb8fb21122b2fb2a026b7e
Jakub-Laska <laska.rayl@gmail.com>
2026-08-24 23:39:27 +0200

    feat: add navbar links make them responsive with burger on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,22 @@ import "../css/Navbar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
-        <a href="" className="navbar-logo-link">
-          <img className="navbar-logo" src="/logo.svg" alt="" />
-        </a>
+      <div className="nav-container">
+        <div className="navbar-brand">
+          <a href="" className="navbar-logo-link">
+            <img className="navbar-logo" src="/nav/logo.svg" alt="" />
+          </a>
+        </div>
+        <div className="navbar-links">
+          <a className="nav-btn kunsthuis-btn">kunsthuis</a>
+          <a className="nav-btn">events</a>
+          <a className="nav-btn">artists</a>
+          <a className="nav-btn">gallery</a>
+          <button className="nav-burger">
+            <img src="/nav/burger.svg" alt="" />
+          </button>
+        </div>
       </div>
-      <div className="navbar-links"></div>
     </nav>
   );
 }

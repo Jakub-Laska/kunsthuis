564522969d3af416d6f9dc00614eccfc31729775
Jakub-Laska <laska.rayl@gmail.com>
2026-08-25 21:39:03 +0200

    feat: add nav mobile menu

---
+++ README.md
@@ -0,0 +1,4 @@
+mobile dropdown menu
+load z bez przeskoku
+leve de kunst gdzies na linii w navie
+big font small font
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,47 @@
-// import { Link } from "react-router-dom";
-
+import { useState } from "react";
 import "../css/Navbar.css";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
+      <div className={`nav-curtain ${menuOpen ? "active" : ""}`}>
+        <div className="curtain-container">
+          <a href="" className="nav-btn curtain-btn kunsthuis-btn">
+            kunsthuis
+          </a>
+          <a className="nav-btn curtain-btn event-btn">evenementen</a>
+          <a className="nav-btn curtain-btn">kunstenaars</a>
+          <a className="nav-btn curtain-btn">galerie</a>
+          <a className="nav-btn curtain-btn">huisregels</a>
+
+          <button className="nav-exit nav-toggle" onClick={handleClick}>
+            <img src="/nav/exit.svg" alt="" />
+          </button>
+        </div>
+      </div>
+
       <div className="nav-container">
         <div className="navbar-brand">
           <a href="" className="navbar-logo-link">
             <img className="navbar-logo" src="/nav/logo.svg" alt="" />
           </a>
         </div>
+
         <div className="navbar-links">
-          <a className="nav-btn kunsthuis-btn">kunsthuis</a>
-          <a className="nav-btn">events</a>
-          <a className="nav-btn">artists</a>
-          <a className="nav-btn">gallery</a>
-          <button className="nav-burger">
+          <a href="" className="nav-btn kunsthuis-btn">
+            kunsthuis
+          </a>
+          <a className="nav-btn">evenementen</a>
+          <a className="nav-btn">kunstenaars</a>
+          <a className="nav-btn">galerie</a>
+
+          <button className="nav-burger nav-toggle" onClick={handleClick}>
             <img src="/nav/burger.svg" alt="" />
           </button>
         </div>

fbdefe187656483e7acda10552fc0ee086e8db6c
Jakub-Laska <laska.rayl@gmail.com>
2026-08-27 15:12:48 +0200

    feat: grid

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -3,101 +3,25 @@ import "../css/Events.css";
 function Events() {
   return (
     <div className="events">
-      <h1>
-        evenementen
-        <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-        reiciendis ullam aliquam fugit omnis hic eum numquam beatae asperiores
-        consequatur doloremque, culpa quaerat, modi ab mollitia nesciunt nam
-        quae dolorum earum suscipit, assumenda laboriosam? Impedit cupiditate
-        porro ad quis blanditiis quas obcaecati dolore ipsum assumenda,
-        necessitatibus consequuntur quaerat doloribus eius nam vero ratione
-        temporibus illo quos voluptate autem. Voluptatem sapiente a ex nostrum
-        fugiat iste sint itaque recusandae magni nam alias, ratione quos aut
-        tempore ducimus odit ipsa vitae similique eveniet maiores, sequi
-        molestiae quae. Eos possimus, incidunt alias vel a commodi porro libero
-        consectetur quaerat quam tenetur nemo harum aperiam! Consequuntur
-        perspiciatis dicta iusto sunt eaque tempora praesentium officia animi
-        nam laudantium cupiditate omnis natus deleniti ullam necessitatibus odio
-        quod deserunt impedit sed quas nihil blanditiis, dolores qui!
-        Blanditiis, omnis sit quaerat, expedita recusandae reprehenderit totam
-        in vero asperiores veritatis repudiandae alias cumque culpa, optio
-        sapiente illo! Culpa, sunt dolores aut iure expedita voluptates qui
-        ipsam, fuga natus ea cum facere totam. Mollitia sint animi quibusdam
-        pariatur adipisci. Reprehenderit quos, officiis pariatur, atque ipsum
-        repudiandae laborum quod esse ullam rem voluptate eveniet velit, ad
-        adipisci et. Ut temporibus optio beatae? Cupiditate, autem ipsum magni.
-        Delectus, accusamus facilis? Sed dignissimos exercitationem magnam
-        necessitatibus, inventore voluptatibus assumenda amet sequi animi itaque
-        fuga sit quas unde nam eos. Molestiae nemo, voluptate sint aperiam, eius
-        in minus nihil, modi nulla quidem ducimus veniam provident consectetur
-        at aut eveniet inventore esse? Consequatur aliquam autem debitis ea
-        voluptatum alias maxime! Repellendus, accusamus nesciunt! Quod, alias
-        veritatis impedit facilis odit voluptatibus qui doloribus! Delectus
-        voluptate quidem architecto numquam a rem laudantium unde deserunt,
-        consequatur temporibus doloribus facere distinctio necessitatibus ipsum!
-        Minima quo libero minus quasi? Quam incidunt atque tenetur repellat
-        nihil? Perspiciatis explicabo voluptatem vitae necessitatibus, ratione
-        amet nobis ducimus non, iure nisi consequuntur, modi aliquid excepturi!
-        Aliquid eum reiciendis ad tempora aspernatur, officiis voluptas, quas
-        exercitationem modi aliquam nesciunt ab nihil reprehenderit corrupti
-        nobis possimus rerum, quae qui vel architecto saepe explicabo voluptatem
-        odio corporis? Doloribus nesciunt veritatis quidem unde minima delectus
-        exercitationem quibusdam corrupti distinctio placeat porro ut tempora
-        cum, sint doloremque nulla quaerat consectetur provident voluptatum hic
-        corporis quae harum architecto quam? Harum maiores optio provident
-        doloremque! Voluptate rem aliquid labore delectus sint doloribus vel
-        cumque animi reiciendis eligendi fugit ratione ipsum error
-        exercitationem quibusdam suscipit laboriosam quisquam consectetur amet
-        vero qui, hic perferendis quod? Voluptas ratione quaerat quia sequi a
-        provident quam. Provident voluptatem numquam illo ipsa voluptatum
-        distinctio, repellat debitis eaque facere sit, excepturi doloribus
-        accusantium animi odit nulla omnis suscipit odio! Dolorem mollitia eius
-        dicta? Iste voluptatibus repellendus vel sit non velit numquam quos
-        aspernatur ad cum, veritatis delectus illum nisi optio deserunt
-        excepturi cumque odit! Eum voluptatum, modi inventore nobis ex magnam
-        porro assumenda quia velit. Neque possimus, facere eum hic consequuntur
-        nisi, rerum eius nulla ab ea in delectus deserunt vero ipsum vel
-        pariatur explicabo natus cumque sapiente? Suscipit atque non harum
-        reprehenderit soluta. Enim facilis sapiente, numquam soluta dolor porro
-        rerum perspiciatis sit asperiores! Dolores vel quia provident neque
-        error exercitationem non est. Dolore maiores libero illo optio sint rem
-        provident vel ea, tempore ipsum pariatur quisquam voluptatem laboriosam
-        distinctio culpa itaque enim illum, vero asperiores ratione laudantium!
-        Officia placeat ipsam adipisci rem accusantium veritatis soluta, aut,
-        asperiores debitis fugiat sit maiores hic repellat quod corporis maxime
-        ad quidem facere labore iusto a. Qui voluptatum itaque molestias
-        reiciendis aliquam consequatur magni voluptates est non, voluptate
-        corrupti sint eligendi esse tempore illo doloremque officiis harum
-        exercitationem quae, libero reprehenderit suscipit saepe! Ipsa minus,
-        omnis accusamus animi, quisquam, enim accusantium aliquam assumenda
-        veritatis nemo dolor. Inventore accusantium iure consequuntur odio quae
-        aperiam eligendi enim sed accusamus laborum mollitia, doloremque
-        repellat voluptatum maiores unde voluptatibus saepe, eveniet dolorum
-        labore! Cupiditate aliquam recusandae perspiciatis perferendis fugiat
-        itaque quasi, dicta distinctio est soluta modi labore aut eligendi
-        nesciunt voluptatibus dolores? Assumenda, temporibus facere quae eos,
-        culpa corrupti sunt et ut sit qui totam est nobis nesciunt cupiditate
-        deserunt iusto beatae maxime! Non, error! Odio suscipit perferendis
-        repellat rerum sint itaque. Iste eligendi quia quas sed repellendus
-        reprehenderit, ducimus quidem ut id sint, nulla a quisquam debitis omnis
-        aperiam quod repellat libero atque deserunt illum nihil, nemo molestiae
-        vero repudiandae. Perferendis, fugit ipsum cumque placeat similique
-        accusantium totam exercitationem nesciunt quod veniam minus. Amet eius
-        nemo obcaecati quas nihil necessitatibus. Fugit nesciunt numquam ipsam
-        totam recusandae voluptas dolores eos porro. Ad, possimus recusandae
-        alias, doloribus tempora molestias molestiae a, accusamus enim iste
-        natus non suscipit quae laboriosam? Autem possimus quia error eaque
-        tenetur accusantium, nobis itaque optio fugit amet aspernatur. Tenetur
-        quasi assumenda aspernatur quisquam iste quas excepturi reiciendis
-        neque, dolores alias illo optio voluptatem voluptas reprehenderit
-        distinctio labore quis consequuntur officiis natus autem quibusdam?
-        Vitae esse veniam asperiores tempora excepturi saepe iusto iste ea
-        pariatur nesciunt error sequi quam magnam, ut debitis dolorum omnis
-        beatae, distinctio odio explicabo quia nemo, assumenda eum odit?
-        Similique quod nam nesciunt excepturi eum ad asperiores omnis beatae,
-        totam tenetur voluptatum sed laudantium recusandae facere blanditiis
-        voluptate!
-      </h1>
+      <div className="grid-container">
+        <div className="grid-left grid-column">
+          <div className="grid-element"></div>
+          <div className="grid-element"></div>
+          <div className="grid-element"></div>
+        </div>
+        <div className="grid-mid grid-column">
+          <div className="grid-element"></div>
+          <div className="grid-element"></div>
+          <div className="grid-element"></div>
+        </div>
+        <div className="grid-right grid-column">
+          <div className="grid-element"></div>
+          <div className="grid-element"></div>
+          <div className="grid-element"></div>
+          <div className="grid-element"></div>
+          <div className="grid-element"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,120 +1,51 @@
 import "../css/Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home">
-      <h1>
-        HOME
-        <br />
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
-        dicta mollitia inventore quidem, vel dolore, sapiente asperiores, nisi
-        exercitationem nulla quaerat? Quia maiores possimus, ab iusto nulla
-        ratione laboriosam velit? Rerum ipsa facilis distinctio animi, ex
-        numquam molestiae qui exercitationem minima aspernatur modi eveniet
-        perferendis. Veniam perferendis cumque unde aperiam quaerat officiis,
-        sunt in, eveniet, accusamus blanditiis nulla aliquid sed! Perspiciatis
-        assumenda culpa corrupti inventore ea repudiandae! Rem fugit culpa qui
-        temporibus ullam quo nihil velit iure quae officia provident vitae,
-        necessitatibus ab porro officiis, eos consequatur nulla fugiat
-        blanditiis eveniet vel consequuntur? Quasi iste temporibus nesciunt,
-        fuga pariatur quidem velit. Porro consequatur, sed facere esse, ad
-        consequuntur in ipsam possimus doloribus debitis vero inventore officiis
-        dicta reprehenderit eum! Sapiente eaque, perferendis error quisquam
-        dolor quibusdam facere? Nostrum ducimus earum doloremque tempore
-        facilis, modi omnis temporibus porro. Neque deleniti consequatur ipsa
-        omnis totam optio sapiente tenetur reiciendis fuga. Recusandae velit
-        laborum tenetur cumque illum aperiam tempore adipisci sequi hic
-        similique reiciendis, quidem quod optio, reprehenderit amet corrupti
-        modi repudiandae ea impedit voluptatum culpa. Officiis sit tenetur
-        dolorum debitis deleniti sed in vitae perspiciatis? Totam consequuntur a
-        dicta quos illum dignissimos praesentium iste molestias deleniti et
-        consequatur itaque autem cupiditate hic, minima delectus sit rerum alias
-        doloremque iure est, excepturi voluptate fugit. Unde nobis animi
-        incidunt officia facere reiciendis nam vitae deleniti natus optio non
-        quo iure suscipit fugit modi, rem dolore aperiam temporibus tempora quam
-        sed dolorum distinctio? Architecto corporis nihil quis expedita qui
-        quidem aperiam nulla voluptas consequuntur corrupti harum fuga, adipisci
-        laboriosam, quod velit iste sit ex eligendi fugit alias rem voluptatum
-        molestias quo. Quae eaque soluta ex quidem laudantium, quaerat, omnis
-        repellat cum laboriosam enim sunt numquam magni tempore deserunt. Non
-        natus blanditiis quidem velit expedita unde sequi fuga molestias ex
-        aliquam perspiciatis adipisci libero iure assumenda eligendi, id
-        exercitationem cupiditate quam aperiam omnis suscipit, nulla repellendus
-        excepturi! Non incidunt nobis amet in sed dolore ipsum accusantium vero
-        nemo delectus omnis quidem, iste quam maiores molestias harum nesciunt
-        aut? Nulla sint quae sapiente eos voluptatum optio harum quia architecto
-        modi debitis error illo ducimus ex voluptas placeat animi accusamus sit
-        nesciunt earum iste asperiores, ea aliquam saepe. Fuga sint aspernatur
-        officia praesentium sit obcaecati hic est cupiditate similique
-        recusandae quibusdam repellat nobis iusto eveniet quam eius dolores,
-        ipsa magnam, ex suscipit minus deleniti. Dolores obcaecati, nisi
-        voluptatem quaerat laborum porro adipisci ratione cupiditate animi
-        debitis corporis aut! Porro, dignissimos! Corrupti ut laboriosam dolores
-        perferendis, facilis veniam nesciunt repellat corporis ratione itaque
-        voluptatibus possimus accusamus natus dicta accusantium dignissimos,
-        expedita eum fuga reiciendis laborum inventore quod blanditiis est
-        provident. Facere ipsum excepturi facilis optio numquam eos quis omnis,
-        eius nisi quaerat neque laborum aliquam atque maxime autem ducimus qui
-        eveniet fugiat tempore. Aut natus facilis fuga excepturi eaque,
-        obcaecati accusantium officia pariatur architecto corporis alias id
-        quibusdam veniam temporibus odio quidem reiciendis. Optio voluptatum sed
-        incidunt, ipsa eveniet, ut accusamus illum, a natus eum repudiandae.
-        Iure cupiditate sunt beatae debitis minima error voluptatem sequi,
-        dolore rem! Officiis numquam, est beatae quas nobis ea quia ex eum
-        maxime consequuntur id reiciendis nulla culpa, voluptas a? Molestias
-        recusandae ex error obcaecati hic dicta libero, iste voluptates eius
-        sunt, quam qui incidunt. Facere ipsa reiciendis magni necessitatibus
-        commodi architecto, doloremque cum quisquam eum animi, eaque debitis
-        totam provident autem earum mollitia cumque aut! Magni modi et maiores
-        veniam ullam dolorem ab necessitatibus? Ut id commodi molestiae ullam
-        voluptatum, veniam consectetur nulla ad aspernatur alias quos! Impedit
-        dolore expedita illum veniam facere sed ipsa error fugiat maiores
-        molestias, quis repellat! Eos reiciendis laudantium atque odit. Eaque a
-        fuga aliquam ratione nemo in id, debitis aliquid temporibus quo amet
-        ducimus odio sequi. Optio reiciendis aliquid ad nostrum doloremque quas
-        quaerat, voluptatibus nesciunt consequuntur laudantium aspernatur
-        deserunt omnis sapiente voluptates dolorum laborum repudiandae ex harum,
-        corporis dignissimos numquam consectetur sit ipsum delectus. Nesciunt
-        fuga aut alias quisquam, itaque quod dicta debitis obcaecati ab,
-        doloribus ex, molestiae repudiandae unde recusandae pariatur est. Rerum
-        est enim, quasi consequuntur quisquam dicta aut unde, odit dignissimos
-        perferendis cupiditate repellat repellendus! Nobis quos eaque neque,
-        accusantium velit natus blanditiis fugit nemo minima quasi sint esse
-        voluptate cum voluptatum ipsa. Dolorem, eius! Ex, enim vitae quaerat
-        velit minus eos reprehenderit praesentium aspernatur mollitia ipsum
-        maxime repellat nam, optio et voluptatem repudiandae harum officiis
-        ducimus? Officia quod voluptatum esse eos quam maxime natus alias! Eos
-        delectus exercitationem suscipit porro, consectetur nihil accusamus
-        rerum molestiae a nulla est in nesciunt quidem impedit tenetur ab quod
-        distinctio saepe nobis. Neque hic illum laudantium fugit tempore
-        delectus deleniti vero ea repudiandae minima inventore quod voluptates
-        dolor culpa omnis dicta, at in! Excepturi non quas quod veniam sapiente
-        sequi atque dolores nesciunt sunt repudiandae modi at doloribus
-        laboriosam culpa aut assumenda eos sint labore itaque facilis amet
-        expedita, quos aperiam. Iusto repudiandae nobis iste architecto,
-        distinctio nemo necessitatibus magnam, animi et hic adipisci inventore
-        nostrum temporibus, placeat voluptatem laborum. Ipsa quidem aut dicta
-        quaerat tempora animi asperiores temporibus quis dolores recusandae
-        dignissimos architecto eaque id harum, voluptas eligendi veritatis nisi
-        expedita exercitationem, placeat nihil non error? Quis, quas quidem.
-        Nemo eius sunt rerum animi amet, labore at consectetur ducimus voluptas
-        deserunt esse aspernatur pariatur accusantium minima molestiae magnam
-        eos! Atque eum impedit adipisci sit totam illum quaerat quas repellat
-        fuga esse doloremque, vel corporis recusandae at dolor minima sint,
-        aspernatur nostrum, fugiat possimus non quia? Recusandae, quidem esse
-        officiis illum molestias porro repudiandae veritatis culpa impedit
-        officia nulla soluta. Eligendi distinctio nihil iure harum nemo
-        excepturi, fugiat quidem quo fuga, deleniti accusantium? Tempora quae
-        cupiditate quis magnam nobis ipsum ea minima minus, vitae repellendus
-        repellat fuga debitis eaque! Quo libero rem, fugiat voluptatem maiores
-        earum, distinctio ea odio modi, numquam possimus veniam inventore!
-        Dolorum asperiores debitis amet mollitia ab voluptatum eaque harum id
-        at, quam aliquam obcaecati quibusdam omnis alias quis voluptate
-        repudiandae fugiat esse cupiditate eum eligendi voluptates totam
-        laudantium? Ex atque reiciendis eveniet illo. Aliquam illum rerum nemo
-        aut asperiores, ex eos pariatur aspernatur quasi maxime voluptatibus
-        esse soluta neque quidem, dolore explicabo?
-      </h1>
+      <div className="grid-container">
+        <div className="grid-left grid-column">
+          <div className="grid-element">upcoming event</div>
+          <div className="grid-element">x</div>
+          <div className="grid-element">x</div>
+        </div>
+        <div className="grid-mid grid-column">
+          <div className="grid-element">next event</div>
+          <div className="grid-element">event after that</div>
+          <div className="grid-element">all events btn</div>
+        </div>
+        <div className="grid-right grid-column">
+          <div className="grid-element">
+            <p>KUNSTHUIS.</p>
+            <p>LEVE DE KUNST. VRIJ, EIGENZINNIG EN ALTIJD IN BEWEGING.</p>
+          </div>
+          <div className="grid-element">
+            <a
+              href="https://www.facebook.com/pages/Kunsthuis%20Amsterdam/1050494525061831/#"
+              target="_blank"
+              className="footer-social-link hover-animation"
+            >
+              facebook
+            </a>
+          </div>
+          <div className="grid-element">
+            <a
+              href="https://www.instagram.com/kunsthuizen/"
+              target="_blank"
+              className="footer-social-link hover-animation"
+            >
+              instagram
+            </a>
+          </div>
+          <div className="grid-element">
+            <Link to="/rules" className="hover-animation">
+              huisregels
+            </Link>
+          </div>
+          <div className="grid-element">x</div>
+        </div>
+      </div>
     </div>
   );
 }
